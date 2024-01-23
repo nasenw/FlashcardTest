46f99a119f5e91e0ba90827166c0a63bf91bcb55
Nasen Wilson <32120172+nasenw@users.noreply.github.com>
2024-01-22 17:22:13 -0800

    adding base project

---
+++ Flashcard/Flashcard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Flashcard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
