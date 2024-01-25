991f1756baa963f876aa2282631a5a5d515325fa
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2024-01-24 17:32:24 -0800

    Added study section

--- Flashcard/Flashcard/Controllers/HomeController.cs
+++ Flashcard/Flashcard/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Flashcard.Controllers
             return View();
         }
 
+        public IActionResult IAmNotSureOfTheName()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
